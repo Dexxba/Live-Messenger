e011f21ddbdb033bd1bfa34ee6dab8dbf53296ef
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2024-05-13 21:32:05 +0200

    Initial project structure

---
+++ backend/src/main/java/com/live/messenger/backend/LiveMessengerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.live.messenger.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveMessengerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveMessengerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/live/messenger/backend/LiveMessengerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.messenger.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveMessengerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
