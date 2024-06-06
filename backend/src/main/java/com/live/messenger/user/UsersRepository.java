8b6c495ef93d290049b15015bb8f0e79e6b831d4
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2024-06-06 20:04:29 +0200

    Updating the UserRepository.

--- backend/src/main/java/com/live/messenger/user/UsersRepository.java
+++ backend/src/main/java/com/live/messenger/user/UsersRepository.java
@@ -2,8 +2,11 @@ package com.live.messenger.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Integer> {
-    Users findByUsername(String username);
+    Optional<Users> findByUsername(String username);
 }
+
