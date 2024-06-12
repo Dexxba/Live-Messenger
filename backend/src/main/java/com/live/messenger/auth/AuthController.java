cbb1559a83994effdd3aaee16571402e11d19d95
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2024-06-12 18:02:13 +0200

    fix: Add exception handling and change response format in login method
    Added try-catch block to handle ResponseStatusException during the login process. The response now returns a JSON object with the JWT token, enhancing error handling and response consistency.

--- backend/src/main/java/com/live/messenger/auth/AuthController.java
+++ backend/src/main/java/com/live/messenger/auth/AuthController.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collections;
 
 
 @RestController
@@ -17,12 +20,12 @@ public class AuthController {
 
     @PostMapping("/auth/login")
     public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
-        // Invoke the authService to obtain a JWT token
-        String jwtToken = authService.getJwt(loginDto);
-
-        // Create a ResponseEntity with a 200 OK status and the JWT token in the response body
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(jwtToken);
+        try {
+            String jwtToken = authService.getJwt(loginDto);
+            return ResponseEntity.ok(Collections.singletonMap("token", jwtToken));
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getReason());
+        }
     }
 }
+
